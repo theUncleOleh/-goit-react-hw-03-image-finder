da54fc5e31e323f2e6b5081237a82e66d505b93d
Oleh Semennykov <sumcue@gmail.com>
2022-05-06 12:25:34 +0300

    add button max

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,205 +12,214 @@ import React, { Component } from 'react';
  import Button from './Button';
  import ErrorMessage from './ErrorMessage';
  import axiosApi from '../services/services-api';
- class App extends Component {
-   state = {
-     searchQuery: null,
-     pictures: [],
-     loading: false,
-     error: null,
-     status: 'idle',
-     largeImageURL: '',
-     page: 1,
-   };
+import { upload } from '@testing-library/user-event/dist/upload';
+class App extends Component {
+  state = {
+    searchQuery: null,
+    pictures: [],
+    loading: false,
+    error: null,
+    status: 'idle',
+    largeImageURL: '',
+    page: 1,
+    upload: 0,
+  };
 
-   handleFormSubmit = query => {
-     // console.log(image);
-     this.setState({ searchQuery: query, page: 1 });
-   };
+  handleFormSubmit = query => {
+    // console.log(image);
+    this.setState({ searchQuery: query, page: 1 });
+  };
 
-   onImageClick = largeImageURL => {
-     this.setState({
-       largeImageURL,
-     });
-     // console.log(largeImageURL);
-   };
+  onImageClick = largeImageURL => {
+    this.setState({
+      largeImageURL,
+    });
+    // console.log(largeImageURL);
+  };
 
-   modalClose = () => {
-     this.setState({
-       largeImageURL: '',
-     });
-   };
+  modalClose = () => {
+    this.setState({
+      largeImageURL: '',
+    });
+  };
 
-   handleLoadButtonClick = () => {
-     console.log('click the button', this.state.page);
+  handleLoadButtonClick = () => {
+    console.log('click the button', this.state.page);
 
-     this.setState(prevState => {
-       return { page: prevState.page + 1 };
-     });
-   };
+    this.setState(prevState => {
+      return { page: prevState.page + 1 };
+    });
+  };
 
-   async componentDidUpdate(prevProps, prevState) {
-     console.log('before', prevState.searchQuery);
-     console.log('after', this.state.searchQuery);
-     if (prevState.searchQuery !== this.state.searchQuery) {
-       this.setState({ status: 'pending', pictures: [] });
-       this.fetchImages();
-     }
-     // await axios
-     //   .get(
-     //     `?q=${newImage}&page=1&key=24437827-e20f686b1c65a4a2859f17630&image_type=photo&orientation=horizontal&per_page=12`
-     //   )
-     // servicesApi
-     //   .axiosApi(newImage, this.state.page)
-     //   .then(response =>
-     //     this.setState({ pictures: response.data.hits, status: 'resolved' })
-     //   )
-     //   .catch(error => {
-     //     this.setState({ error, status: 'rejected' });
-     //   });
+  async componentDidUpdate(prevProps, prevState) {
+    console.log('before', prevState.searchQuery);
+    console.log('after', this.state.searchQuery);
+    if (prevState.searchQuery !== this.state.searchQuery) {
+      this.setState({ status: 'pending', pictures: [] });
+      this.fetchImages();
+    }
+    // await axios
+    //   .get(
+    //     `?q=${newImage}&page=1&key=24437827-e20f686b1c65a4a2859f17630&image_type=photo&orientation=horizontal&per_page=12`
+    //   )
+    // servicesApi
+    //   .axiosApi(newImage, this.state.page)
+    //   .then(response =>
+    //     this.setState({ pictures: response.data.hits, status: 'resolved' })
+    //   )
+    //   .catch(error => {
+    //     this.setState({ error, status: 'rejected' });
+    //   });
 
-     // if (newPage === 1) {
-     //   this.setState({ status: 'pending' });
-     //   servicesApi
-     //     .axiosApi(newImage, newPage)
-     //     .then(response =>
-     //       this.setState({ pictures: response.data.hits, status: 'resolved' })
-     //     )
-     //     .catch(error => {
-     //       this.setState({ error, status: 'rejected' });
-     //     });
-     // }
-     if (prevState.page !== this.state.page) {
-       this.fetchImages();
-     }
+    // if (newPage === 1) {
+    //   this.setState({ status: 'pending' });
+    //   servicesApi
+    //     .axiosApi(newImage, newPage)
+    //     .then(response =>
+    //       this.setState({ pictures: response.data.hits, status: 'resolved' })
+    //     )
+    //     .catch(error => {
+    //       this.setState({ error, status: 'rejected' });
+    //     });
+    // }
+    if (prevState.page !== this.state.page) {
+      this.fetchImages();
+    }
 
-     //   if (prevPage !== newPage) {
-     //     this.setState({ status: 'pending' });
-     //     servicesApi
-     //       .axiosApi(newImage, newPage)
-     //       .then(response =>
-     //         this.setState(state => ({
-     //           pictures: [...state.pictures, ...response.data.hits],
-     //           status: 'resolved',
-     //         }))
-     //       )
-     //       .catch(error => {
-     //         this.setState({ error, status: 'rejected' });
-     //       });
-     //   }
-     // }
-   }
-   fetchImages = async () => {
-     const { searchQuery, page } = this.state;
+    //   if (prevPage !== newPage) {
+    //     this.setState({ status: 'pending' });
+    //     servicesApi
+    //       .axiosApi(newImage, newPage)
+    //       .then(response =>
+    //         this.setState(state => ({
+    //           pictures: [...state.pictures, ...response.data.hits],
+    //           status: 'resolved',
+    //         }))
+    //       )
+    //       .catch(error => {
+    //         this.setState({ error, status: 'rejected' });
+    //       });
+    //   }
+    // }
+  }
+  fetchImages = async () => {
+    const { searchQuery, page } = this.state;
 
-     try {
-       const data = await axiosApi({ searchQuery, page });
+    try {
+      const data = await axiosApi({ searchQuery, page });
 
-       this.setState(prevState => ({
-         pictures: [...prevState.pictures, ...data],
-         status: 'resolved',
-         newPage: data.length,
-       }));
-     } catch (error) {
-       this.setState({ error, status: 'rejected' });
-     }
-   };
+      this.setState(prevState => ({
+        pictures: [...prevState.pictures, ...data],
+        status: 'resolved',
+        newPage: data.length,
+      }));
+    } catch (error) {
+      this.setState({ error, status: 'rejected' });
+    }
+  };
 
-   // componentDidUpdate(prevProps, prevState) {
-   //   const prevImage = prevState.image;
-   //   const newImage = this.state.image;
-   //   if (prevImage !== newImage) {
-   //     console.log('new picture');
-   //     // this.setState({ loading: true, pictures: [] });
-   //     this.setState({ status: 'pending' });
+  // componentDidUpdate(prevProps, prevState) {
+  //   const prevImage = prevState.image;
+  //   const newImage = this.state.image;
+  //   if (prevImage !== newImage) {
+  //     console.log('new picture');
+  //     // this.setState({ loading: true, pictures: [] });
+  //     this.setState({ status: 'pending' });
 
-   //     fetch(
-   //       `https://pixabay.com/api/?q=${newImage}&page=1&key=24437827-e20f686b1c65a4a2859f17630&image_type=photo&orientation=horizontal&per_page=12`
-   //     )
-   //       .then(response => {
-   //         if (response.ok) {
-   //           return response.json();
-   //         }
-   //         return Promise.reject(new Error('Картинки с таким названием нет'));
-   //       })
-   //       .then(data =>
-   //         this.setState({ pictures: data.hits, status: 'resolved' })
-   //       )
-   //       .catch(error => this.setState({ error, status: 'rejected' }));
-   //     // .finally(this.setState({ loading: false }));
-   //   }
-   // }
+  //     fetch(
+  //       `https://pixabay.com/api/?q=${newImage}&page=1&key=24437827-e20f686b1c65a4a2859f17630&image_type=photo&orientation=horizontal&per_page=12`
+  //     )
+  //       .then(response => {
+  //         if (response.ok) {
+  //           return response.json();
+  //         }
+  //         return Promise.reject(new Error('Картинки с таким названием нет'));
+  //       })
+  //       .then(data =>
+  //         this.setState({ pictures: data.hits, status: 'resolved' })
+  //       )
+  //       .catch(error => this.setState({ error, status: 'rejected' }));
+  //     // .finally(this.setState({ loading: false }));
+  //   }
+  // }
 
-   render() {
-     const { error, pictures, status, largeImageURL } = this.state;
+  render() {
+    const { error, pictures, status, largeImageURL, upload } = this.state;
+    const totalPictures = pictures.length;
+    console.log(totalPictures);
+    if (status === 'idle') {
+      return (
+        <div>
+          <SearchBar onSubmit={this.handleFormSubmit} />
+          <NotificationMessage />
+          <ToastContainer position="top-right" autoClose={5000} />
+        </div>
+      );
+    }
 
-     if (status === 'idle') {
-       return (
-         <div>
-           <SearchBar onSubmit={this.handleFormSubmit} />
-           <NotificationMessage />
-           <ToastContainer position="top-right" autoClose={5000} />
-         </div>
-       );
-     }
+    if (status === 'pending') {
+      return <Loader />;
+    }
 
-     if (status === 'pending') {
-       return <Loader />;
-     }
+    if (status === 'rejected') {
+      return <ErrorMessage message={error.message} />;
+    }
 
-     if (status === 'rejected') {
-       return <ErrorMessage message={error.message} />;
-     }
+    if (status === 'resolved') {
+      return (
+        <div className={s.app}>
+          <SearchBar onSubmit={this.handleFormSubmit} />
+          <ImageGallery pictures={pictures} onClick={this.onImageClick} />
+          {/* {pictures.length > 0 && (
+             <Button onClick={this.handleLoadButtonClick} />
+           )} */}
+          {totalPictures > 0 && totalPictures >= 12 && (
+            <Button onClick={this.handleLoadButtonClick} />
+          )}
 
-     if (status === 'resolved') {
-       return (
-         <div className={s.app}>
-           <SearchBar onSubmit={this.handleFormSubmit} />
-           <ImageGallery pictures={pictures} onClick={this.onImageClick} />
-           {pictures && <Button onClick={this.handleLoadButtonClick} />}
-           {largeImageURL.length > 0 && (
-             <Modal onClose={this.modalClose}>
-               <button
-                 type="button"
-                 className={s.buttonModal}
-                 onClick={this.modalClose}
-               >
-                 <AiOutlineCloseCircle />
-               </button>
-               <img
-                 src={this.state.largeImageURL}
-                 alt=""
-                 width="100%"
-                 height="100%"
-               />
-             </Modal>
-           )}
-         </div>
-       );
-     }
-     // return (
-     //   <div className={s.app}>
-     //     {!image && <div>tell me what you want</div>}
-     //     {error && <h2>{error.message}</h2>}
-     //     {loading && <div>Loading......</div>}
-     //     <SearchBar onSubmit={this.handleFormSubmit} />
-     //     <ImageGallery pictures={pictures} />
-     //     {pictures && <button className={s.button}>Load more</button>}
+          {largeImageURL.length > 0 && (
+            <Modal onClose={this.modalClose}>
+              <button
+                type="button"
+                className={s.buttonModal}
+                onClick={this.modalClose}
+              >
+                <AiOutlineCloseCircle />
+              </button>
+              <img
+                src={this.state.largeImageURL}
+                alt=""
+                width="100%"
+                height="100%"
+              />
+            </Modal>
+          )}
+        </div>
+      );
+    }
+    // return (
+    //   <div className={s.app}>
+    //     {!image && <div>tell me what you want</div>}
+    //     {error && <h2>{error.message}</h2>}
+    //     {loading && <div>Loading......</div>}
+    //     <SearchBar onSubmit={this.handleFormSubmit} />
+    //     <ImageGallery pictures={pictures} />
+    //     {pictures && <button className={s.button}>Load more</button>}
 
-     //     <ToastContainer
-     //       position="top-right"
-     //       autoClose={5000}
-     //       hideProgressBar={false}
-     //       newestOnTop={false}
-     //       closeOnClick
-     //       rtl={false}
-     //       pauseOnFocusLoss
-     //       draggable
-     //       pauseOnHover
-     //     />
-     //   </div>
-     // );
-   }
- }
+    //     <ToastContainer
+    //       position="top-right"
+    //       autoClose={5000}
+    //       hideProgressBar={false}
+    //       newestOnTop={false}
+    //       closeOnClick
+    //       rtl={false}
+    //       pauseOnFocusLoss
+    //       draggable
+    //       pauseOnHover
+    //     />
+    //   </div>
+    // );
+  }
+}
 
 export default App;
